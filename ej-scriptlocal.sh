ba1748765391da75c3a2ed6232c87695f7be405a
Alejo Nahuel Gariglio <gariglio.alejo@gmail.com>
2016-06-24 18:42:30 +0000

    cambios para que no limite el num allign

--- ej-scriptlocal.sh
+++ ej-scriptlocal.sh
@@ -1,1 +1,1 @@
-/home/manager/bio/ncbi-blast-2.4.0+/bin/blastp -db /home/manager/bio/swissprot -num_alignments 10 -query /home/manager/bio/input/CYP11B1_mRNA_traducido.fasta -out /home/manager/bio/out/ej2_blastreport.out
+/home/manager/bio/ncbi-blast-2.4.0+/bin/blastp -db /home/manager/bio/swissprot -query /home/manager/bio/input/CYP11B1_mRNA_traducido.fasta -out /home/manager/bio/out/ej2_blastreport.out
